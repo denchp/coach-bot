f6991985631222fcb2a83065975ef8932e95f1e9
Patrick Dench <denchp@gmail.com>
2020-02-08 16:04:57 -0600

    Bot inserted

---
+++ bot/index.js
@@ -0,0 +1,59 @@
+const tmi = require('tmi.js');
+const commands = require('./commands');
+
+const { BOT_USERNAME, OAUTH_TOKEN, CHANNEL_NAME } = require('./.env.js');
+
+// Define configuration options
+const opts = {
+  identity: {
+    username: BOT_USERNAME,
+    password: OAUTH_TOKEN
+  },
+  channels: [
+    CHANNEL_NAME
+  ]
+};
+
+// Create a client with our options
+const client = new tmi.client(opts);
+
+// Register our event handlers (defined below)
+client.on('message', onMessageHandler);
+client.on('connected', onConnectedHandler);
+
+// Connect to Twitch:
+client.connect();
+
+// Called every time a message comes in
+function onMessageHandler (target, context, msg, self) {
+  if (self) { return; } // Ignore messages from the bot
+
+
+  // Remove whitespace from chat message
+  const commandName = msg.trim();
+
+  if (commandName[0] != '!')
+    return;
+
+    const commandArray = commandName.split(' ');
+
+    if (commands[commandArray[0]]) {
+        commands[commandArray[0]](client, target, context, commandArray.filter((v, i) => i > 0));
+    } else {
+        console.log(`${commandName}: is borked`);
+    }
+}
+
+// Called every time the bot connects to Twitch chat
+function onConnectedHandler (addr, port) {
+    console.log(`* Connected to ${addr}:${port}`);
+}
+
+const coachBot = {
+    getData: url => {
+        const split = url.split('/');
+        return commands[split[2]][split[3]]();
+    }
+}
+
+module.exports = coachBot;
