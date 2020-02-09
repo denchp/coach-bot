fc57653b74cd5b8f5bcf57ebf6f3f6b734a019a4
Patrick Dench <denchp@gmail.com>
2020-02-09 11:25:48 -0600

    WIP

--- bot/commands/index.js
+++ bot/commands/index.js
@@ -10,7 +10,7 @@ module.exports = {
             return(audioCmd(audio[cmd]));
         
         if (['!hi', '!hello', 'o/' ].some(s => s === cmd))
-            return(greetings(name));
+            return(greetingCmd(name));
     }
 }
 
