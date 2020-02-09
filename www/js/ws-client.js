5f7046da90dc259d8a173f44d90bf9e7b2443ec7
Patrick Dench <denchp@gmail.com>
2020-02-08 22:40:35 -0600

    WIP

--- www/js/ws-client.js
+++ www/js/ws-client.js
@@ -4,17 +4,23 @@ ws.addEventListener('open', function () {
   ws.send('something');
 });
 
-ws.addEventListener('message', function (data) {
-  if(data.type === 'audio') {
-    playAudio(data);
-  }
+ws.addEventListener('message', function (raw) {
+    let data = raw;
+
+    try {
+        data = JSON.parse(raw);
+    } catch {}
+
+    if(data.type === 'audio') {
+        playAudio(data);
+    }
 });
 
 const playAudio = async config => {
     window.AudioContext = window.AudioContext || window.webkitAudioContext;
     var context = new AudioContext();
     
-    const resp = await fetch(`/audio/${config.file}`)
+    const resp = await fetch(`/audio/${config.file}`);
     const file = await resp.arrayBuffer();
 
     const audioBuffer = context.decodeAudioData(file);
