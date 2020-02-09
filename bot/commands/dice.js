3bfc7ca24fe9154157d3a0d9db5f3c42e68b4326
Patrick Dench <denchp@gmail.com>
2020-02-08 20:38:27 -0600

    Added user name to dice command

--- bot/commands/dice.js
+++ bot/commands/dice.js
@@ -1,7 +1,7 @@
 const func = (client, target, context, args) => {
     const sides = args[0] || 6;
     const result = Math.floor(Math.random() * sides) + 1;
-    client.say(target, `You rolled a ${sides} sided die and got a ${result}`);
+    client.say(target, `${context.username} rolled a ${sides} sided die and got a ${result}`);
 }
 
 func.description = 'role a n sided die ("!dice 4" for a 4-sided die)'
