e87d0d158dcf834e41fb5418e7e1f87c9f933075
Patrick Dench <denchp@gmail.com>
2020-02-09 00:03:58 -0600

    WIP

--- bot/commands/audio.js
+++ bot/commands/audio.js
@@ -1,6 +1,19 @@
+timeouts = {
+    userName: { commandName: true }
+};
+
 module.exports = config => (
-    (client, target, context, args, messageHandler) => messageHandler({
-        type: 'audio',
-        file: config.file
-    })
+    (client, target, context, args, messageHandler) => { 
+        if (timeouts[context.username] && timeouts[context.username][config.file])
+            return;
+        
+        timeouts[context.username] = { ...context.username, [config.file]: true };
+
+        setTimeout(() => delete timeouts[context.username][config.file], 10000);
+        
+        messageHandler({
+            type: 'audio',
+            file: config.file
+        })
+    }
 );
