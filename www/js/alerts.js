aaa111e98668bd1d01f0b983e1097f2c44eda836
Patrick Dench <denchp@gmail.com>
2020-02-18 10:22:15 -0600

    .js ...

--- www/js/alerts.js
+++ www/js/alerts.js
@@ -1,4 +1,4 @@
-exports.subscriber = data => {
+export const subscriber = data => {
     const video = document.getElementById("alert-video")
     video.src = '/video/subscriber_alert.webm';
     video.preload();
