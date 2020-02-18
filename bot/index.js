38620642c6df7b7192634f5f10cce9fd09967e28
Patrick Dench <denchp@gmail.com>
2020-02-18 10:08:12 -0600

    Logging messages

--- bot/index.js
+++ bot/index.js
@@ -28,7 +28,7 @@ client.connect();
 // Called every time a message comes in
 function onMessageHandler (target, context, msg, self) {
   if (self) { return; } // Ignore messages from the bot
-  console.log(`Parsing message: ${msp}`);
+  console.log(`Parsing message: ${msg}`);
 
   // Remove whitespace from chat message
   const commandName = msg.trim();
