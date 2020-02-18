e4b60e027e5b17c2fc9ebd69d22b7fedd125004f
Patrick Dench <denchp@gmail.com>
2020-02-18 14:47:31 -0600

    Import -> require

--- bot/commands/index.js
+++ bot/commands/index.js
@@ -1,6 +1,6 @@
 const audioCmd = require('./audio');
 const greetingCmd = require('./greetings');
-const test = require('./test');
+const test = require('./test/index.js');
 console.log(JSON.stringify(test));
 
 module.exports = {
