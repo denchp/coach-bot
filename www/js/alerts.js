2708c16110389a111776cc24f9846cc013b95f68
Patrick Dench <denchp@gmail.com>
2020-02-18 22:26:04 -0600

    Added audio delay in config

--- www/js/alerts.js
+++ www/js/alerts.js
@@ -10,7 +10,7 @@ export const subscriber = async data => {
     video.src = '/video/subscriber_alert.webm';
     playAudio({
         file: data.audio,
-        delay: 500
+        delay: 600
     })
 
     setTimeout(() => {
