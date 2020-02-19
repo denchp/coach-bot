93fed26af66d9ad2fb8af0c8928a1807c5e8b359
Patrick Dench <denchp@gmail.com>
2020-02-18 20:47:21 -0600

    Added label

--- bot/index.js
+++ bot/index.js
@@ -33,22 +33,22 @@ client.connect();
 // Called every time a message comes in
 function onMessageHandler (target, context, msg, self) {
   if (self) { return; } // Ignore messages from the bot
-  console.log(`Parsing message: ${msg}`);
 
   // Remove whitespace from chat message
   const commandName = msg.trim();
 
   if (commandName[0] != '!' && commandName[0] != 'o')
     return;
+  console.log(`Parsing message: ${msg}`);
+  
+  const commandArray = commandName.split(' ');
+  const cmd = commands.getCommand(commandArray[0], context.username);
 
-    const commandArray = commandName.split(' ');
-    const cmd = commands.getCommand(commandArray[0], context.username);
-
-    if (cmd) {
-        cmd(client, target, context, commandArray.slice(1), coachBot.onMessage);
-    } else {
-        console.log(`${commandName}: is borked`);
-    }
+  if (cmd) {
+      cmd(client, target, context, commandArray.slice(1), coachBot.onMessage);
+  } else {
+      console.log(`${commandName}: is borked`);
+  }
 }
 
 // Called every time the bot connects to Twitch chat
--- www/js/alerts.js
+++ www/js/alerts.js
@@ -1,11 +1,14 @@
 export const subscriber = data => {
     const video = document.getElementById("alert-video");
+    const container = document.getElementById("alert-container");
+    const label = document.getElementById("alert-label");
 
-    video.classList.remove('hide');
-
+    container.classList.remove('hide');
+    label.innerHTML = data.name;
+    
     video.src = '/video/subscriber_alert.webm';
     
     setTimeout(() => video.play(), 500);
 
-    video.addEventListener('ended' ,() => video.classList.add('hide'));
+    video.addEventListener('ended' ,() => container.classList.add('hide'));
 }
