b6d1835ef3045c542b084aea1c451cea30703cba
Patrick Dench <denchp@gmail.com>
2020-02-18 21:04:39 -0600

    CSS changes

--- www/js/alerts.js
+++ www/js/alerts.js
@@ -1,11 +1,11 @@
 export const subscriber = data => {
     const video = document.getElementById("alert-video");
     const container = document.getElementById("alert-container");
-    const label = document.getElementById("alert-label");
+    const labels = document.getElementsByClassName("alert-label");
 
     container.classList.remove('hide');
-    label.innerHTML = data.name;
-    
+    labels.forEach(e => e.setAttribute('title', data.userName));
+
     video.src = '/video/subscriber_alert.webm';
     
     setTimeout(() => video.play(), 500);
