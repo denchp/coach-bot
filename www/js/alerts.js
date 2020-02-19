3e2396fee586aca6bccdab6c021f179de45db96e
Patrick Dench <denchp@gmail.com>
2020-02-18 20:23:01 -0600

    CSS changes

--- www/js/alerts.js
+++ www/js/alerts.js
@@ -1,6 +1,10 @@
 export const subscriber = data => {
-    const video = document.getElementById("alert-video")
+    const video = document.getElementById("alert-video");
+
+    video.classList.remove('hide');
+
     video.src = '/video/subscriber_alert.webm';
-    // video.preload();
     video.play();
+
+    video.onended = () => { video.classList.add('hide'); }
 }
