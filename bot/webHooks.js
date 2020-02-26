83140c7cfefd35401134166d5fb22d22f425a01c
Patrick Dench <denchp@gmail.com>
2020-02-25 21:59:17 -0600

    Started subs

--- bot/webHooks.js
+++ bot/webHooks.js
@@ -4,8 +4,8 @@ const whl = require('twitch-webhooks').default;
 const { CLIENT_ID, ACCESS_TOKEN } = process.env;
 
 const initHooks = async () => {
-    const client = TwitchClient.withClientCredentials(CLIENT_ID, ACCESS_TOKEN);
-    const user = await client.helix.users.getUserByName('Pestily');
+    const client = TwitchClient.withCredentials(CLIENT_ID, ACCESS_TOKEN);
+    const user = await client.helix.users.getUserByName('summit1g');
 
     const listener = await whl.create(client, { port: 8090 });
     listener.listen();
@@ -13,8 +13,8 @@ const initHooks = async () => {
     let followers = await listener.subscribeToFollowsToUser(user, (event) => { console.log(JSON.stringify(event)); });
     followers.start();
 
-    let subs = await listener.subscribeToSubscriptionEvents(user, (event) => { console.log(JSON.stringify(event)); });
-    subs.start();
+    // let subs = await listener.subscribeToSubscriptionEvents(user, (event) => { console.log(JSON.stringify(event)); });
+    // subs.start();
 }
 
 initHooks();
