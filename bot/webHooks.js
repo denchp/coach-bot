a59cef86fc576e192d7ccbb5002277bfe618644f
Patrick Dench <denchp@gmail.com>
2020-02-29 20:49:25 -0600

    Sorta working

--- bot/webHooks.js
+++ bot/webHooks.js
@@ -5,13 +5,18 @@ const { CLIENT_ID, ACCESS_TOKEN } = process.env;
 
 const initHooks = async () => {
     const client = TwitchClient.withCredentials(CLIENT_ID, ACCESS_TOKEN);
-    const user = await client.helix.users.getUserByName('summit1g');
+    const user = await client.helix.users.getUserByName('GLend2');
 
-    const listener = await whl.create(client, { port: 8090 });
+    const listener = await whl.create(client, {
+        hostName: 'coachdench-bot.herokuapp.com',
+        port: 80,
+    });
+
     listener.listen();
 
-    let followers = await listener.subscribeToFollowsToUser(user, (event) => { console.log(JSON.stringify(event)); });
-    followers.start();
+    let followers = await listener.subscribeToFollowsToUser(user, async (event) => { console.log(JSON.stringify(event)); });
+
+    console.log(`Listening for followers...`);
 
     // let subs = await listener.subscribeToSubscriptionEvents(user, (event) => { console.log(JSON.stringify(event)); });
     // subs.start();
