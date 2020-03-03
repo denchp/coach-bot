4a2e56473185380bdd8d645712c805146881de20
Patrick Dench <denchp@gmail.com>
2020-03-02 21:06:43 -0600

    Follow tweaks

--- bot/commands/text.js
+++ bot/commands/text.js
@@ -1,5 +1,8 @@
 module.exports = {
     '!portal3': (client, target, context, args) => {
     client.say(target, `watch a wild @CoachDench wait for valve to make a Portal 3 ;) https://cdn.discordapp.com/attachments/657736161955151935/684224140403933197/unknown.png`);
+    },
+    '!command': (client, target, context, args) => {
+        client.say(target, `please see !help for command info`);
     }
 }
