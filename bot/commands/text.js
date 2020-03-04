9f3b713a0bd53ad92d103dfb0997a165b0065071
Patrick Dench <denchp@gmail.com>
2020-03-03 20:43:47 -0600

    WIP

--- bot/commands/text.js
+++ bot/commands/text.js
@@ -1,8 +1,8 @@
 module.exports = {
     '!portal3': (client, target, context, args) => {
-    client.say(target, `watch a wild @CoachDench wait for valve to make a Portal 3 ;) https://cdn.discordapp.com/attachments/657736161955151935/684224140403933197/unknown.png`);
+    client.say(target, `watch a wild @CoachDench wait for valve to make a Portal 3 ;) https://cdn.discordapp.com/attachments/657736161955151935/684224140403933197/unknown.png (Custom command from glEnd2)`);
     },
     '!command': (client, target, context, args) => {
-        client.say(target, `please see !help for command info`);
+        client.say(target, `please see !help for command info. (Custom command from glEnd2)`);
     }
 }
