053fee9bff23e52dd728860965869d9810886bac
Patrick Dench <denchp@gmail.com>
2020-05-02 22:14:30 -0500

    WWD commment

--- bot/webHooks.js
+++ bot/webHooks.js
@@ -30,7 +30,7 @@ const initHooks = async (messageHandler) => {
     });
 
     console.log(`Listening for followers...`);
-
+    // Ducks was here (WhiteWithDuck 5/2/2020)
     let subs = await listener.subscribeToSubscriptionEvents(user, (event) => {
         console.log(JSON.stringify(event));
         typeof messageHandler === 'function' ? messageHandler({ type: 'newSubscriber', ...event }) : console.log(`No message handler for event`);
