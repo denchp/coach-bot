ae2ae4480fa953031b1694a2d2b680457a0643dd
Patrick Dench <denchp@gmail.com>
2020-05-18 22:41:39 -0500

    whargarbl

--- bot/commands/audio.js
+++ bot/commands/audio.js
@@ -1,25 +1,25 @@
 const timeouts = {
-    userName: { commandName: true }
+    username: { commandName: true }
 };
 
 module.exports = config => (
     (client, target, context, args, messageHandler) => {
-        if(timeouts[context.userName] && timeouts[context.userName].audio || timeouts[config.file]) {
+        if(timeouts[context.username] && timeouts[context.username].audio || timeouts[config.file]) {
             console.log(`Not executing command:
-                ${ timeouts[context.userName] && timeouts[context.userName].audio ? context.userName + " is on cool down" :
+                ${ timeouts[context.username] && timeouts[context.username].audio ? context.username + " is on cool down" :
                 config.file + " is on cool down"
              }`);
             return;
         }
 
-        if (timeouts[context.userName] && timeouts[context.userName][config.file]) {
+        if (timeouts[context.username] && timeouts[context.username][config.file]) {
             console.log(`Cool down hit`);
-            client.whisper(context.userName, `Sorry, that command is on cool down...`);
+            client.whisper(context.username, `Sorry, that command is on cool down...`);
             return;
         }            
         
-        timeouts[context.userName] = {
-            ...timeouts[context.userName], 
+        timeouts[context.username] = {
+            ...timeouts[context.username], 
             [config.file]: true,
             audio: true
         };
@@ -28,8 +28,8 @@ module.exports = config => (
             timeouts[config.file] = true;
 
 
-        setTimeout(() => { console.log(context.userName, ' off timeout'); timeouts[context.userName].audio = undefined }, 5000);
-        setTimeout(() => timeouts[context.userName][config.file] = undefined, config.timeout || 30000);
+        setTimeout(() => { console.log(context.username, ' off timeout'); timeouts[context.username].audio = undefined }, 5000);
+        setTimeout(() => timeouts[context.username][config.file] = undefined, config.timeout || 30000);
         setTimeout(() => timeouts[config.file] = undefined, config.globalTimeout || 30000);
 
         messageHandler && messageHandler({
