55fc097286f6cf6d7da2b3b6d57dc21f456e2a73
Patrick Dench <denchp@gmail.com>
2020-05-18 22:39:33 -0500

    whargarbl

--- bot/commands/audio.js
+++ bot/commands/audio.js
@@ -4,7 +4,7 @@ const timeouts = {
 
 module.exports = config => (
     (client, target, context, args, messageHandler) => {
-        if(timeouts[context.username] && timeouts[context.username].audio || timeouts[config.file]) {
+        if(timeouts[context.userName] && timeouts[context.userName].audio || timeouts[config.file]) {
             console.log(`Not executing command:
                 ${ timeouts[context.userName] && timeouts[context.userName].audio ? context.userName + " is on cool down" :
                 config.file + " is on cool down"
@@ -12,14 +12,14 @@ module.exports = config => (
             return;
         }
 
-        if (timeouts[context.username] && timeouts[context.username][config.file]) {
+        if (timeouts[context.userName] && timeouts[context.userName][config.file]) {
             console.log(`Cool down hit`);
-            client.whisper(context.username, `Sorry, that command is on cool down...`);
+            client.whisper(context.userName, `Sorry, that command is on cool down...`);
             return;
         }            
         
-        timeouts[context.username] = {
-            ...timeouts[context.username], 
+        timeouts[context.userName] = {
+            ...timeouts[context.userName], 
             [config.file]: true,
             audio: true
         };
@@ -28,8 +28,8 @@ module.exports = config => (
             timeouts[config.file] = true;
 
 
-        setTimeout(() => timeouts[context.username].audio = undefined, 5000);
-        setTimeout(() => timeouts[context.username][config.file] = undefined, config.timeout || 30000);
+        setTimeout(() => { console.log(context.userName, ' off timeout'); timeouts[context.userName].audio = undefined }, 5000);
+        setTimeout(() => timeouts[context.userName][config.file] = undefined, config.timeout || 30000);
         setTimeout(() => timeouts[config.file] = undefined, config.globalTimeout || 30000);
 
         messageHandler && messageHandler({
