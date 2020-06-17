8ffbe21cd77d83044df46049acdf58906c8fc1f8
Patrick Dench <denchp@gmail.com>
2020-06-16 20:36:40 -0500

    WIP

--- bot/commands/test.js
+++ bot/commands/test.js
@@ -1,7 +1,7 @@
 const subEvents = require('../subEvents.js');
 
 exports['!testSub'] = (client, channel, user, args, messageHandler) => {
-    if (!context.badges || !context.badges.broadcaster)
+    if (!user !== 'coachdench')
         return;
 
     subEvents.newSubscriber(args[0], messageHandler);
