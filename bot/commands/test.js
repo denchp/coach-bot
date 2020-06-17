813d09275cb08a60371bc0d6d5c6c89b554cd39a
Patrick Dench <denchp@gmail.com>
2020-06-16 20:46:43 -0500

    Subs working! maybe?

--- bot/commands/test.js
+++ bot/commands/test.js
@@ -1,7 +1,7 @@
 const subEvents = require('../subEvents.js');
 
 exports['!testSub'] = (client, channel, user, args, messageHandler) => {
-    if (!user !== 'coachdench')
+    if (user !== 'coachdench')
         return;
 
     subEvents.newSubscriber(args[0], messageHandler);
